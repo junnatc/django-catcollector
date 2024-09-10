9fe00eb980b6c5b593e954a9a30cb48ed525467e
junnatc <junnatchowdhury@gmail.com>
2024-09-10 12:09:09 -0400

    updates

---
+++ main_app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# Define the home view
+class Home(APIView):
+  def get(self, request):
+    content = {'message': 'Welcome to the cat-collector api home route!'}
+    return Response(content)
+  
+from rest_framework import generics
+from .models import Cat
+from .serializers import CatSerializer
+
+class CatList(generics.ListCreateAPIView):
+  queryset = Cat.objects.all()
+  serializer_class = CatSerializer
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Cat.objects.all()
+  serializer_class = CatSerializer
+  lookup_field = 'id'
